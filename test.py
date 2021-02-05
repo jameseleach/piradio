555611d19a4e14d7b79ee3b261e02d12043d7831
James E. Leach <james.e.leach@gmail.com>
2021-02-05 13:29:04 -0600

    change to csv

--- test.py
+++ test.py
@@ -8,13 +8,17 @@ from luma.oled.device import ssd1327
 
 from PIL import Image, ImageFont, ImageDraw
 
+import csv
+
 # Set default volume
 current_volume = 10
 current_channel = 4
 mute_status = False
 
+
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
 
 # Initialize screen
 serial = i2c(port=1, address=0x3D)
@@ -27,15 +31,19 @@ for i in keys:
     volume_graphic[i] = Image.open('gfx/vdt-' + str(i) + '.jpg')
 volume_graphic_mute = Image.open('gfx/vdt-mute.jpg')
 
-# Setup channel graphic dict
-channel_graphic = {}
-keys = range(12)
-for i in keys:
-    channel_graphic[i] = Image.open('gfx/c-' + str(i) + '.jpg')
-
 device.display(volume_graphic[current_volume])
 
+channels = {}
+with open('channels.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        i = reader.line_num -1
+        channels[i] = {'Name': row['Name'], 'Description': row['Description'], 'StreamURL': row['StreamURL'], 'Graphic': Image.open('gfx/' + row['Graphic'])}
+        # print(row['Channel'], row['Name'],row['Description'], row['StreamURL'])
+
 # Setup volume buttons
+
+
 def mute():
     global mute_status
     if mute_status == True:
@@ -45,6 +53,7 @@ def mute():
         device.display(volume_graphic_mute)
         mute_status = True
     print(f"Mute status: {mute_status}")
+
 
 def vol_rotate():
     global current_volume
@@ -57,6 +66,7 @@ def vol_rotate():
         print(f"Current volume: {current_volume}")
         device.display(volume_graphic[current_volume])
 
+
 button_vol_select = Button(4, pull_up=True)
 button_vol_select.when_pressed = mute
 
@@ -65,16 +75,16 @@ button_vol_dn = Button(6, pull_up=True, bounce_time=0.1)
 button_vol_up.when_activated = vol_rotate
 
 # Setup channel buttons
+
+
 def ch_rotate():
     global current_channel
     if not button_ch_dn.is_pressed:
-        current_channel = clamp(current_channel + 1, 0, len(channel_graphic)-1)
-        print(f"Current channel: {current_channel}")
-        device.display(channel_graphic[current_channel])
+        current_channel = clamp(current_channel + 1, 0, len(channels)-1)
     else:
-        current_channel = clamp(current_channel - 1, 0, len(channel_graphic)-1)
-        print(f"Current channel: {current_channel}")
-        device.display(channel_graphic[current_channel])
+        current_channel = clamp(current_channel - 1, 0, len(channels)-1)
+    print(f"Current channel: {current_channel}")
+    device.display(channels[current_channel]['Graphic'])
 
 button_ch_select = Button(22, pull_up=True)
 button_ch_select.when_pressed = mute

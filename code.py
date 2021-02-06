9ef1ad6f0802deeb3ab15a399e9b2072e13413e7
James E. Leach <james.e.leach@gmail.com>
2021-02-06 17:10:54 -0600

    correct channel index

--- code.py
+++ code.py
@@ -16,21 +16,25 @@ current_volume = 10
 current_channel = 4
 mute_status = False
 
+
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
 
 # Initialize screen
 serial = i2c(port=1, address=0x3D)
 device = ssd1327(serial)
 
-# Setup volume graphic dict
+# Setup volume graphics
+volume_graphic_mute = Image.open('gfx/vdt-mute.jpg')
 volume_graphic = {}
 keys = range(21)
 for i in keys:
     volume_graphic[i] = Image.open('gfx/vdt-' + str(i) + '.jpg')
-volume_graphic_mute = Image.open('gfx/vdt-mute.jpg')
 
-# Setup channels dict
+
+# Setup channel graphics
+c_standby = Image.open('gfx/c-standby.jpg')
 channels = {}
 with open('channels.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -42,15 +46,14 @@ with open('channels.csv', newline='') as csvfile:
         }
         print(
             f"Channel: {channels[index]['Name']} URL: {channels[index]['StreamURL']}")
-c_standby = Image.open('gfx/c-standby.jpg')
+
 
 # Setup volume buttons
-
 def mute():
     global mute_status
     if mute_status == True:
         device.display(volume_graphic[current_volume])
-        os.system("mpc volume " + str(current_volume * 5) )
+        os.system("mpc volume " + str(current_volume * 5))
         mute_status = False
     else:
         device.display(volume_graphic_mute)
@@ -58,18 +61,19 @@ def mute():
         os.system("mpc volume 0")
     print(f"Mute status: {mute_status}")
 
+
 def vol_rotate():
     global current_volume
     if not button_vol_dn.is_pressed:
-        new_volume = clamp(current_volume + 1, 0, len(volume_graphic)-1)
-        current_volume = 
+        new_volume = clamp(current_volume + 1, 0, len(volume_graphic) - 1)
     else:
-        new_volume = clamp(current_volume - 1, 0, len(volume_graphic))
+        new_volume = clamp(current_volume - 1, 0, len(volume_graphic) - 1)
     print(f"Current volume: {current_volume}")
     if not new_volume == current_volume:
         current_volume = new_volume
-        os.system("mpc volume " + str(current_volume * 5) )
+        os.system("mpc volume " + str(current_volume * 5))
         device.display(volume_graphic[current_volume])
+
 
 button_vol_select = Button(4, pull_up=True)
 button_vol_select.when_pressed = mute
@@ -91,16 +95,18 @@ def change_channel(current_channel):
     device.display(channels[current_channel]['Graphic'])
     os.system("mpc play")
 
+
 def ch_rotate():
     global current_channel
     if not button_ch_dn.is_pressed:
-        new_channel = clamp(current_channel + 1, 1, len(channels)-1)
+        new_channel = clamp(current_channel + 1, 0, len(channels) - 1)
     else:
-        new_channel = clamp(current_channel - 1, 1, len(channels)-1)
+        new_channel = clamp(current_channel - 1, 0, len(channels) - 1)
     print(f"Current channel: {channels[current_channel]['Name']}")
     if not new_channel == current_channel:
         current_channel = new_channel
         change_channel(current_channel)
+
 
 button_ch_select = Button(22, pull_up=True)
 button_ch_select.when_pressed = mute

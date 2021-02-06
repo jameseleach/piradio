510aaa18ce3841055ed0d86c24ad8bf16ae2c2f8
James E. Leach <james.e.leach@gmail.com>
2021-02-06 13:54:01 -0600

    graphic changes

--- code.py
+++ code.py
@@ -8,14 +8,17 @@ from luma.oled.device import ssd1327
 from PIL import Image, ImageFont, ImageDraw
 
 import csv
+import random
 
 # Set default volume
 current_volume = 10
 current_channel = 4
 mute_status = False
 
+
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
 
 # Initialize screen
 serial = i2c(port=1, address=0x3D)
@@ -28,18 +31,22 @@ for i in keys:
     volume_graphic[i] = Image.open('gfx/vdt-' + str(i) + '.jpg')
 volume_graphic_mute = Image.open('gfx/vdt-mute.jpg')
 
-# Show default volume
-device.display(volume_graphic[current_volume])
-
+# Setup channels dict
 channels = {}
 with open('channels.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        i = reader.line_num -1
-        channels[i] = {'Name': row['Name'], 'Description': row['Description'], 'StreamURL': row['StreamURL'], 'Graphic': Image.open('gfx/' + row['Graphic'])}
-        print(i, row['Name'],row['Description'], row['StreamURL'])
+    for index, row in enumerate(reader):
+        channels[index] = {
+            'Name': row['Name'],
+            'StreamURL': 'http://ice' + str(random.randint(1, 6)) + '.somafm.com/' + row['Channel'] + '-128-aac',
+            'Graphic': Image.open('gfx/c-' + row['Channel'] + ".jpg")
+        }
+        print(
+            f"Channel: {channels[index]['Name']} URL: {channels[index]['StreamURL']}")
 
 # Setup volume buttons
+
+
 def mute():
     global mute_status
     if mute_status == True:
@@ -49,6 +56,7 @@ def mute():
         device.display(volume_graphic_mute)
         mute_status = True
     print(f"Mute status: {mute_status}")
+
 
 def vol_rotate():
     global current_volume
@@ -61,6 +69,7 @@ def vol_rotate():
         print(f"Current volume: {current_volume}")
         device.display(volume_graphic[current_volume])
 
+
 button_vol_select = Button(4, pull_up=True)
 button_vol_select.when_pressed = mute
 
@@ -69,6 +78,8 @@ button_vol_dn = Button(6, pull_up=True)
 button_vol_up.when_activated = vol_rotate
 
 # Setup channel buttons
+
+
 def ch_rotate():
     global current_channel
     if not button_ch_dn.is_pressed:
@@ -78,11 +89,18 @@ def ch_rotate():
     print(f"Switching to {channels[current_channel]['Name']}")
     device.display(channels[current_channel]['Graphic'])
 
+
 button_ch_select = Button(22, pull_up=True)
 button_ch_select.when_pressed = mute
 
 button_ch_up = Button(23, pull_up=True)
 button_ch_dn = Button(24, pull_up=True)
 button_ch_up.when_activated = ch_rotate
+
+# Show default volume
+# device.display(volume_graphic[current_volume])
+
+# Show default channel
+device.display(channels[current_channel]['Graphic'])
 
 pause()
